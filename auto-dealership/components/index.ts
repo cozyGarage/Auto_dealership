775a6b746bd2bdb3d5e7dcfde6c58f19adaf0124
cozyGarage <e1500953@edu.vamk.fi>
2023-06-08 16:06:02 +0300

    fix bugs

--- auto-dealership/components/index.ts
+++ auto-dealership/components/index.ts
@@ -1,13 +1,23 @@
-
-import NavBar from './NavBar';
-import Footer from './Footer';
 import Hero from './Hero';
 import CustomButton from './CustomButton';
-
+import NavBar from './NavBar';
+import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
+import ShowMore from './ShowMore';
 
 export {
-  CustomButton,
   Hero,
+  CustomButton,
   NavBar,
   Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
 }
