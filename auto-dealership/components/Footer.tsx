ef10028f5faca44d4eac4f599e0d723e3cb785ac
cozyGarage <e1500953@edu.vamk.fi>
2023-06-08 15:55:30 +0300

    Main layout

---
+++ auto-dealership/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";  
+import { footerLinks } from "@/constants"; 
+
+const Footer = () => {
+  return (
+    <footer>
+        footer
+    </footer>
+  )
+}
+
+export default Footer
---
+++ auto-dealership/components/index.ts
@@ -0,0 +1,13 @@
+
+import NavBar from './NavBar';
+import Footer from './Footer';
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+
+
+export {
+  CustomButton,
+  Hero,
+  NavBar,
+  Footer,
+}
